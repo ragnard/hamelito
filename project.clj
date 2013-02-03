72f6de4fb8000bb3760fcd532541d5d15820c2aa
Ragnar Dahlén <r.dahlen@gmail.com>
2013-02-03 21:35:23 +0000

    Generate tests from haml-spec, currently only tests if haml parses ok

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [hiccup "1.0.2"]
-                 [org.blancas/kern "0.5.0"]])
+                 [org.blancas/kern "0.5.0"]
+                 [cheshire "5.0.1"]])
---
+++ test/hamlite/haml_spec_test.clj
@@ -0,0 +1,59 @@
+(ns hamlite.haml-spec-test
+  (:require [cheshire.core :as json]
+            [clojure.string :as string]
+            [clojure.java.io :as io]
+            [hamlite.comb :as haml])
+  (:use [clojure.test :only [deftest is testing]]))
+
+(def haml-spec (json/parse-stream
+                (io/reader "resources/haml-spec/tests.json")))
+
+(def included-categories
+  #{
+    "basic Haml tags and CSS"
+    ;;   "boolean attributes"
+    ;;   "conditional comments"
+    ;;   "headers"
+    ;;   "HTML escaping"
+    ;;   "internal filters"
+    ;;   "markup comments"
+    ;;   "Ruby-style interpolation"
+    ;;   "silent comments"
+    ;;   "tags with HTML-style attributes"
+    ;;   "tags with Ruby-style attributes"
+    "tags with inline content"
+    "tags with nested content"
+    "tags with unusual CSS identifiers"
+    ;;   "tags with unusual HTML characters"
+    ;;   "whitespace preservation"
+    ;;   "whitespace removal"
+    })
+
+(defn- category->test-name
+  [category]
+  (-> category
+      (string/replace #"\s" "-")
+      string/lower-case
+      (str "-test")
+      symbol))
+
+(defn- test-case
+  [[description test-data]]
+  (let [haml (test-data "haml")]
+    `(testing ~description
+       (let [res# (haml/parse-haml ~haml)]
+         (is (:ok res#) (str "Input: \n" ~haml "\n\n"))
+         (is (= nil (seq (:input res#))))))))
+
+(defmacro generate-parse-tests
+  []
+  `(do 
+     ~@(for [[category tests] haml-spec
+             :when (contains? included-categories category)]
+         (let [name  (category->test-name category)
+               tests (map test-case tests)]
+           `(deftest ~name
+              ~@tests)))))
+
+
+(generate-parse-tests)
