0cc0ffa61b5f5bb95b0a933240d6ffe7a68b09df
Ragnar Dahlén <r.dahlen@gmail.com>
2013-02-04 09:20:48 +0000

    Ruby style attributes

--- src/hamlite/parsing.clj
+++ src/hamlite/parsing.clj
@@ -8,14 +8,6 @@
   [char parser]
   (between (sym* char) (sym* char) parser))
 
-(defn double-quoted
-  [parser]
-  (quoted \" parser))
-
-(defn single-quoted
-  [parser]
-  (quoted \' parser))
-
 (defn anything-but
   [char]
   (satisfy #(not= % char)))
@@ -24,6 +16,14 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; HAML parser
 
+;; doctype
+(def doctype-prolog    (token* "!!!" ))
+(def doctype-value     (many (anything-but \newline)))
+(def doctype           (>> doctype-prolog doctype-value))
+(def doctypes          (sep-by (many new-line*) doctype))
+
+;; general
+
 (def identifier (<|> alpha-num (sym* \-) (sym* \_)))
 
 (defn prefixed-identifier
@@ -31,15 +31,20 @@
   (>> (token* prefix)
       (<+> (many1 identifier))))
 
+
 (def element           (prefixed-identifier "%"))
 (def id                (prefixed-identifier "#"))
 (def class             (prefixed-identifier "."))
 
-;; attributes
+
+
+;;;; attributes
 
 (defn quoted-any
   [char]
   (quoted char (<+> (many (anything-but char)))))
+
+;; html-style attributes
 
 (def html-name         (<+> (many1 identifier)))
 (def html-value        (<|> (<+> (many1 identifier))
@@ -52,19 +57,38 @@
 
 (def open-paren        (sym* \())
 (def close-paren       (sym* \)))
-
-
-
 (def html-attributes   (between open-paren close-paren html-attr-pairs))
 
-(def attributes        html-attributes)
+;; ruby-style attributes
 
-;; doctype
+(def ruby-keyword      (<+> (sym* \:) (many1 identifier)))
 
-(def doctype-prolog    (token* "!!!" ))
-(def doctype-content   (many (anything-but \newline)))
-(def doctype           (>> doctype-prolog doctype-content))
-(def doctypes          (sep-by (many new-line*) doctype))
+(def ruby-name         (<|> ruby-keyword
+                            (quoted-any \")
+                            (quoted-any \')))
+
+(def ruby-value        (<|> (quoted-any \")
+                            (quoted-any \')))
+
+(def ruby-attr-pair    (<*> ruby-name
+                            (many white-space)
+                            (token* "=>")
+                            (many white-space)
+                            ruby-value))
+
+(def ruby-attr-sep     (<*> (many white-space) (sym* \,) (many white-space)))
+
+(def ruby-attr-pairs   (sep-by ruby-attr-sep ruby-attr-pair))
+
+(def open-curly        (sym* \{))
+(def close-curly       (sym* \}))
+(def ruby-attributes   (between open-curly close-curly
+                                (<*> (many white-space)
+                                     ruby-attr-pairs
+                                     (many white-space))))
+
+(def attributes        (<|> html-attributes ruby-attributes))
+
 
 ;; tag
 
@@ -77,7 +101,8 @@
                              (return {:element el
                                       :id id
                                       :classes (into #{} (concat c1 c2))
-                                      :self-close? (boolean cl)})))
+                                      :self-close? (boolean cl)
+                                      :attributes at})))
                         
 (def text              (<+> (many (anything-but \newline))))
 
@@ -98,15 +123,14 @@
                         
 (def lines             (many line))
                         
-(def haml              (<< (<*> doctypes lines) eof))
-
-(println "------------------------------------------------------------------------")
+(def haml              (<< lines eof))
+;; (def haml              (<< (<*> doctypes lines) eof))
 
 ;(run* element "%d")
 
 (comment
 
-  (run* tag  "%div.a.b.c#d42.e.f")
+  (run* tag  "!!! XML\n\n%div.a.b.c#d42.e.f")
 
   (run* tag-line  "  %div.a.b.c#d42.e.f")
 
@@ -118,7 +142,7 @@
 
   (run* haml "%p(a='b'\n  c='d')")
 
-  (pprint (run* haml "%div\n  %div\n    %p\n      text"))
+  (run* haml "%div\n  %div\n    %p\n      text")
   
   )
 
