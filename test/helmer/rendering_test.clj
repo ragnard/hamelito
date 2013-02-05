a3249c538876c2ddcb2221a4f6c59f1c83b7fb83
Ragnar Dahlén <r.dahlen@gmail.com>
2013-02-05 13:59:08 +0000

    Few more tests for elements with combinations of id, classes and tags

--- test/helmer/rendering_test.clj
+++ test/helmer/rendering_test.clj
@@ -8,7 +8,6 @@
      (are [input# expected#] (= expected# (r/to-html input#))
           ~@test-cases)))
 
-
 (defhtmltest basic
 
   ""
@@ -41,6 +40,33 @@
   "%p Foo Bar"
   "<p>Foo Bar</p>")
 
+(defhtmltest elements
+  "%x"
+  "<x />"
+
+  "#a"
+  "<div id=\"a\"></div>"
+
+  ".a"
+  "<div class=\"a\"></div>"
+
+  ".a.b"
+  "<div class=\"a b\"></div>"
+
+  "#a.b.c"
+  "<div class=\"b c\" id=\"a\"></div>"
+
+  "%x#a.b.c"
+  "<x class=\"b c\" id=\"a\" />"
+
+  "%x.a.b#c.d.e"
+  "<x class=\"a b d e\" id=\"c\" />"
+
+  "%x.a.b#c.d.e Blahonga"
+  "<x class=\"a b d e\" id=\"c\">Blahonga</x>"
+  
+  )
+
 (defhtmltest nested-elements
 
   "%div\n  %div"
@@ -53,4 +79,8 @@
   "<div><div></div></div><div></div>"
 
   "%div Hello There\n  %div Stranger!\n%div More Stuff"
-  "<div>Hello There<div>Stranger!</div></div><div>More Stuff</div>")
+  "<div>Hello There<div>Stranger!</div></div><div>More Stuff</div>"
+
+
+  
+  )
