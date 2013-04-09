7043dd7dd28789ca797e0f5d0e058a395485ab49
Ragnar Dahlén <r.dahlen@gmail.com>
2013-04-09 16:47:07 +0100

    Bump kern to 0.6.1

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [hiccup "1.0.2"]
-                 [org.blancas/kern "0.5.0"]
+                 [org.blancas/kern "0.6.1"]
                  [cheshire "5.0.1"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0-RC15"]]}
